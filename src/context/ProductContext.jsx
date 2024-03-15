ba69096e5c7099ad96d1ee1f083291de78023366
Mehrsa <mhrs.project22@gmail.com>
2024-03-15 14:11:30 +0330

    create context for products

---
+++ src/context/ProductContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useEffect, useState } from "react";
+import api from "../services/config";
+
+const ProductContext = createContext();
+
+function ProductsProvider({ children }) {
+    //keep fetched data in this state
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await api.get("/products");
+                setProducts(response);
+            } catch (error) {
+                console.log(error.message);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+
+    return (
+        <>
+            <ProductContext.Provider value={products}>
+                {children}
+            </ProductContext.Provider>
+        </>
+    );
+}
+
+export default ProductsProvider;
